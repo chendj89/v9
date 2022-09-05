96e3c9094e41d39c594c344922d1de55804181fa
chendj89 <chendj89@hotmail.com>
2022-09-05 21:46:56 +0800

    333

---
+++ utils/copyFiles.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const fs = require("fs");
+const ProgressBar = require("progress");
+function copyFiles(opts) {
+  // 判断目录是否存在
+  // 没有就创建
+  if (!fs.existsSync(opts.dest)) {
+    fs.mkdirSync(opts.dest);
+  }
+  // 判断来源是否存在
+  if (fs.existsSync(opts.src) == false) {
+    return false;
+  }
+  if (opts.src.startsWith(process.cwd())) {
+    console.log(`不能自己复制给自己`);
+    return false;
+  }
+  let dirs = fs.readdirSync(opts.src);
+  var bar = new ProgressBar(`${opts.description}：[:bar] :percent :etas`, {
+    total: dirs.length,
+  });
+  new Promise((resolve) => {
+    for (let i = 0; i < dirs.length; i++) {
+      let item = dirs[i];
+      let item_path = path.join(opts.src, item);
+      let temp = fs.statSync(item_path);
+      if (temp.isFile()) {
+        fs.copyFileSync(item_path, path.join(opts.dest, item));
+        bar.tick();
+      }
+    }
+    resolve(true);
+  });
+  bar = null;
+}
+module.exports = copyFiles;
